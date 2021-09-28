c55d714478337596ece3e233090fe5ada4726d24
mohammmad1997 <ghadamnan88@gmail.com>
2021-09-28 13:07:16 +0330

    ph2

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.http import HttpResponse
+def hello(request):
+    return render(request,'blog/hello.html',{})
 
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
